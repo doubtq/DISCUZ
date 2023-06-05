4d88eb1a9c4bcac7fdce31fa3de21f76511c699b
doubtq <1687331402@qq.com>
2023-06-05 23:47:40 +0800

    删除文件

--- code/com-project/crm/src/main/java/com/bjpowernode/crm/commors/contants/Contants.java
+++
@@ -1,8 +0,0 @@
-package com.bjpowernode.crm.commors.contants;
-
-public class Contants {
-
-    public static final String RETURN_OBJECT_CODE_SUCCESS="1";
-    public static final String RETURN_OBJECT_CODE_FALL="0";
-
-}
